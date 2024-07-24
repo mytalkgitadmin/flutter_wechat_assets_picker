16b6d69107fb92e1e97cbb8da677cef0afcbc7ef
swom.bemily <swom@bemily.com>
2024-07-24 13:32:17 +0900

    feat : AppBar Test

--- lib/src/widget/asset_picker_app_bar.dart
+++ lib/src/widget/asset_picker_app_bar.dart
@@ -264,3 +264,35 @@ class AssetPickerAppBarWrapper extends StatelessWidget {
     );
   }
 }
+
+class AssetPickerLFAppBarWrapper extends StatelessWidget {
+  const AssetPickerLFAppBarWrapper({
+    super.key,
+    required this.appBar,
+    required this.body,
+  });
+
+  final PreferredSizeWidget appBar;
+  final Widget body;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      type: MaterialType.transparency,
+      child: Stack(
+        children: <Widget>[
+          Positioned.fill(
+            top:
+                MediaQuery.paddingOf(context).top + appBar.preferredSize.height,
+            child: MediaQuery.removePadding(
+              context: context,
+              removeTop: true,
+              child: body,
+            ),
+          ),
+          Positioned.fill(bottom: null, child: appBar),
+        ],
+      ),
+    );
+  }
+}
